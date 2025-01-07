50073c59b0fa052647e93da3629318ed0b862f2a
Konstantina Vasileva <konstantinavasileva@gmail.com>
2025-01-07 14:47:12 +0200

    Starting my project

---
+++ src/main/java/bg/softuni/Jukebox/model/entity/User.java
@@ -0,0 +1,28 @@
+package bg.softuni.Jukebox.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String confirmPassword;
+}
