b20c3476b915e31538b7650fd654d0929e0dd890
Konstantina Vasileva <konstantinavasileva@gmail.com>
2025-01-10 19:10:13 +0200

    Create login style

--- src/main/java/bg/softuni/Jukebox/controller/UserController.java
+++ src/main/java/bg/softuni/Jukebox/controller/UserController.java
@@ -1,15 +1,29 @@
 package bg.softuni.Jukebox.controller;
 
+import bg.softuni.Jukebox.service.CurrentUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 //@RequestMapping("/users")
 public class UserController {
 
+    private final CurrentUser currentUser;
+
+    public UserController(CurrentUser currentUser) {
+        this.currentUser = currentUser;
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        currentUser.logout();
+        return "redirect:/";
     }
 }
